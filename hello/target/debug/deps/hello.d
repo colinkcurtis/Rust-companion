93ff970f971c70d0d3bd95addbcc01e007752fc3
colinkcurtis <colinkcurtis@gmail.com>
2021-03-16 23:48:27 -0400

    first Rust code, hello_world

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+/Users/colincurtis/Documents/Rust-companion/hello/target/debug/deps/hello: src/main.rs
+
+/Users/colincurtis/Documents/Rust-companion/hello/target/debug/deps/hello.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/colincurtis/Documents/Rust-companion/hello/target/debug/hello: /Users/colincurtis/Documents/Rust-companion/hello/src/main.rs
