6d4efa6cf0d65263080a416674622f4a672a7036
colinkcurtis <colinkcurtis@gmail.com>
2021-03-19 00:19:51 -0400

    remove dir

--- hello/target/debug/deps/hello.d
+++
@@ -1,5 +0,0 @@
-/Users/colincurtis/Documents/Rust-companion/hello/target/debug/deps/hello: src/main.rs
-
-/Users/colincurtis/Documents/Rust-companion/hello/target/debug/deps/hello.d: src/main.rs
-
-src/main.rs:
--- hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/Users/colincurtis/Documents/Rust-companion/hello/target/debug/hello: /Users/colincurtis/Documents/Rust-companion/hello/src/main.rs
